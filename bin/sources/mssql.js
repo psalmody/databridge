37e2af33f2dc69bd08311e0eeebb04cda6c8c896
Michael Smith <psalmody@gmail.com>
2016-04-05 11:52:30 -0800

    npm deploy

--- bin/sources/mssql.js
+++ bin/sources/mssql.js
@@ -15,15 +15,8 @@ module.exports = function(options, spinner, moduleCallback) {
     db = options.source,
     prependFile = require('prepend-file');
 
-  //request timeout for mssql database
-  /*creds.options = {
-    requestTimeout: -1
-  };
-  creds.requestTimeout = -1;*/
-
   async.waterfall([
     function(cb) {
-      console.log(creds);
       mssql.connect(creds).then(function() {
         log.group('mssql').log('connected to mssql');
         cb(null);
