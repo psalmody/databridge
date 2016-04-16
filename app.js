83b26835c434e1371d9d5afb97129d862a0394a2
Michael Smith <psalmody@gmail.com>
2016-04-15 16:57:07 -0800

    Simplify app and move program help to module

--- app.js
+++ app.js
@@ -6,191 +6,108 @@
 //assume we're in development
 process.env.NODE_ENV = typeof(process.env.NODE_ENV) == 'undefined' ? 'development' : process.env.NODE_ENV;
 
-var program = require('commander'),
+var program = require('./bin/cli'),
   async = require('async'),
   colors = require('colors'),
   fs = require('fs'),
   bridge = require('./bin/bridge'),
   missingKeys = require('./bin/missing-keys'),
-  pkg,
-  sources = [],
-  destinations = [],
-  batches = [],
-  config = require('./config/' + process.env.NODE_ENV);
-
-console.log(config);
-
-async.waterfall([
-    //get package.json (for version exposure)
-    function(cb) {
-      //console.log('read package.json');
-      fs.readFile('package.json', 'utf-8', function(err, contents) {
-        if (err) return cb(err);
-        pkg = JSON.parse(contents);
-        cb(null);
-      })
-    },
-    //get valid sources
-    function(cb) {
-      fs.readdir(config.dirs.sources, function(err, files) {
-        if (err) return cb(err);
-        //sources exclude .js extensions
-        for (var i = 0; i < files.length; i++) {
-          sources.push(files[i].replace('.js', ''));
-        }
-        cb(null);
-      })
-    },
-    //get valid destinations
-    function(cb) {
-      fs.readdir(config.dirs.destinations, function(err, files) {
-        if (err) return cb(err);
-        //again - exclude .js extension
-        for (var i = 0; i < files.length; i++) {
-          destinations.push(files[i].replace('.js', ''));
-        }
-        cb(null);
-      })
-    },
-    //get batches
-    function(cb) {
-      fs.readdir(config.dirs.batches, function(err, files) {
-        if (err) return cb(err);
-        for (var i = 0; i < files.length; i++) {
-          batches.push(files[i].replace('.json', ''));
-        }
-        cb(null);
-      })
-    },
-    //setup commander
-    function(cb) {
-      var newline = '\n                                 ';
-      program.version(pkg.version)
-        .usage('[options]')
-        .option('-s, --source [source]', 'Specify source from bin/sources/. ' + newline + 'Currently installed: ' + sources.join(', '))
-        .option('-t, --table [table]', 'Specify query or input file name (no file extension). ' + newline + 'Try --source <source> and --show for a list of inputs ' + newline + 'for that source.')
-        .option('-d, --destination [destination]', 'Specify destination from bin/destinations/. ' + newline + 'Current installed: ' + destinations.join(', '))
-        .option('-h, --show', 'Shows valid sources/destinations/tables.')
-        .option('-b, --binds', 'Use default binds from input/binds.js rather than prompting ' + newline + '(applies to some sources only).')
-        .option('-k, --task', 'This command is a schedule task or piping to output file.' + newline + 'Don\'t generate processing spinner.')
-        .option('--batch [batch]', 'Run json [batch] from batches/')
-        .option('-u, --update', 'Update table (insert data only) don\'t overwrite ' + newline + 'existing table or update table structure (for database destinations).')
-        .on('--help', function() {
-          console.log('  Examples: ');
-          console.log('');
-          console.log('    Run dbutil for rptp query employees into mysql:');
-          console.log('    > node app -s rptp -t employees -d mysql');
-          console.log('');
-          console.log('    Show all valid sources:');
-          console.log('    > node app -hs');
-          console.log('');
-          console.log('    Show all valid destinations:');
-          console.log('    > node app -hd');
-          console.log('');
-          console.log('    Run batch of data bridges:');
-          console.log('    > node app --batch batchplease');
-          console.log('');
-          console.log('    Show all valid tables for that source:');
-          console.log('    > node app -s <source> -ht');
-        })
-        .parse(process.argv);
-      //parse command options for
-      //show valid source table options
-      if (missingKeys(program, ['source', 'table', 'show']) == false) {
-        fs.readdir(config.dirs.input + program.source, function(err, files) {
-          if (err) return cb(err);
-          var valid = [];
-          for (var i = 0; i < files.length; i++) {
-            if (files[i].indexOf('.') !== 0) valid.push(files[i].replace(/.sql|.js|.csv|.txt/g, ''));
-          }
-          console.log('Valid tables for ' + program.source + ':');
-          console.log('  ' + valid.join('\n  '));
-        })
-        return;
-      }
-      //show valid sources
-      else if (missingKeys(program, ['source', 'show']) == false) {
-        console.log('Valid sources:');
-        console.log('  ' + sources.join('\n  '));
-        process.exit();
-      }
-      //show valid destinations
-      else if (missingKeys(program, ['destination', 'show']) == false) {
-        console.log('Valid destinations:');
-        console.log('  ' + destinations.join('\n  '));
-        process.exit();
-      }
-      //show valid batches
-      else if (missingKeys(program, ['batch', 'show']) == false) {
-        console.log('Valid batches:');
-        console.log('  ' + batches.join(', '));
-        process.exit();
-      }
-      //define array of bridge functions to run
-      var bridges = [];
-      //run bridge batch
-      if (missingKeys(program, ['batch']) == false) {
-        fs.readFile(config.dirs.batches + program.batch + '.json', 'utf-8', function(err, json) {
-          if (err) return cb(err);
-          var batch = JSON.parse(json);
-
-          for (var i = 0; i < batch.length; i++) {
-            var b = batch[i];
-            //set to task by default
-            if (Object.keys(batch[i]).indexOf('task') === -1) batch[i].task = true;
-            batch[i].batch = program.batch;
-            var fn = (function() {
-              var options = JSON.parse(JSON.stringify(b));
-              return function(cb2) {
-                bridge(options, function(err) {
-                  if (err) return cb2(err);
-                  cb2(null);
-                })
-              }
-            })(b);
-            bridges.push(fn);
-          }
-          cb(null, bridges);
-        })
+  pkg = require('./package'),
+  config = require('./config/' + process.env.NODE_ENV),
+  sources = fs.readdirSync(config.dirs.sources),
+  destinations = fs.readdirSync(config.dirs.destinations),
+  batches = fs.readdirSync(config.dirs.batches);
 
 
-      } else {
-        //otherwise, run bridge once
-        //only source / destination are required - each source module should throw
-        //an error if table is necessary
-        var missing = missingKeys(program, ['source', 'destination']);
-        if (missing.length) return cb('Wrong usage.');
-        //push program version
-        bridges.push(function(cb2) {
-          bridge(config, {
-            source: program.source,
-            destination: program.destination,
-            binds: program.binds,
-            table: program.table,
-            task: program.task,
-            update: program.update
-          }, function(err) {
-            if (err) return cb2(err);
-            cb2(null);
-          })
-        })
-        cb(null, bridges);
-      }
-    },
-    //run bridge functions async style
-    function(bridges, cb) {
-      if (!bridges.length) return cb('No bridges found or defined. Check usage or batch file.');
-      //taking all bridge functions created and Running
-      //them one at a time
-      async.waterfall(bridges, function(err) {
-        if (err) return cb(err);
-        cb(null);
-      })
+if (missingKeys(program, ['source', 'table', 'show']) == false) {
+  fs.readdir(config.dirs.input + program.source, function(err, files) {
+    if (err) return cb(err);
+    var valid = [];
+    for (var i = 0; i < files.length; i++) {
+      if (files[i].indexOf('.') !== 0) valid.push(files[i].replace(/.sql|.js|.csv|.txt/g, ''));
     }
-  ],
-  function(err) {
+    console.log('Valid tables for ' + program.source + ':');
+    console.log('  ' + valid.join('\n  '));
+  })
+  return;
+}
+//show valid sources
+else if (missingKeys(program, ['source', 'show']) == false) {
+  console.log('Valid sources:');
+  console.log('  ' + sources.join('\n  '));
+  process.exit();
+}
+//show valid destinations
+else if (missingKeys(program, ['destination', 'show']) == false) {
+  console.log('Valid destinations:');
+  console.log('  ' + destinations.join('\n  '));
+  process.exit();
+}
+//show valid batches
+else if (missingKeys(program, ['batch', 'show']) == false) {
+  console.log('Valid batches:');
+  console.log('  ' + batches.join(', '));
+  process.exit();
+}
+
+//run bridge functions async style
+function runBridges(bridges) {
+  if (!bridges.length) return cb('No bridges found or defined. Check usage or batch file.');
+  //taking all bridge functions created and Running
+  //them one at a time
+  async.waterfall(bridges, function(err) {
     if (err) {
       console.error(colors.red(err));
       program.help();
     }
   })
+}
+
+//define array of bridge functions to run
+var bridges = [];
+//run bridge batch
+if (missingKeys(program, ['batch']) == false) {
+  fs.readFile(config.dirs.batches + program.batch + '.json', 'utf-8', function(err, json) {
+    if (err) return console.error(colors.red(err));
+    var batch = JSON.parse(json);
+
+    for (var i = 0; i < batch.length; i++) {
+      var b = batch[i];
+      //set to task by default
+      if (Object.keys(batch[i]).indexOf('task') === -1) batch[i].task = true;
+      batch[i].batch = program.batch;
+      var fn = (function() {
+        var options = JSON.parse(JSON.stringify(b));
+        return function(cb) {
+          bridge(config, options, function(err) {
+            if (err) return cb(err);
+            cb(null);
+          })
+        }
+      })(b);
+      bridges.push(fn);
+    }
+    runBridges(bridges);
+  })
+} else {
+  //otherwise, run bridge once
+  //only source / destination are required - each source module should throw
+  //an error if table is necessary
+  var missing = missingKeys(program, ['source', 'destination']);
+  if (missing.length) return cb('Wrong usage.');
+  //push program version
+  bridges.push(function(cb) {
+    bridge(config, {
+      source: program.source,
+      destination: program.destination,
+      binds: program.binds,
+      table: program.table,
+      task: program.task,
+      update: program.update
+    }, function(err) {
+      if (err) return cb(err);
+      cb(null);
+    })
+  })
+  runBridges(bridges);
+}
