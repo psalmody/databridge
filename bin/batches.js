aa5e0bb8fe5458343c8c4d5ed496ba3029927194
Michael Smith <psalmody@gmail.com>
2016-04-19 12:22:02 -0800

    move batch generator to module

---
+++ bin/batches.js
@@ -0,0 +1,26 @@
+//batches should return an array of bridge functions to run
+module.exports = function(batchName, batchFile, moduleCallback) {
+  var batch = require(batchFile);
+  var bridge = require('./bridge');
+  var bridges = [];
+  var config = require('../config/' + process.env.NODE_ENV);
+
+  batch.forEach(function(options) {
+    var b = Object.create(options);
+    //always use tasks unless specifically noted as not
+    if (Object.keys(b).indexOf('task') === -1) b.task = true;
+    b.batch = batchName;
+    var fn = (function() {
+      var options = Object.create(b);
+      return function(cb) {
+        bridge(config, b, function(err) {
+          if (err) return cb(err);
+          cb(null)
+        })
+      }
+    })(b);
+    bridges.push(fn);
+  })
+
+  return moduleCallback(bridges);
+}
