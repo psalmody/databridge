96782010a8c89c3044a2a34381ac40ac69eb93ba
Michael Smith <psalmody@gmail.com>
2016-04-19 17:15:18 -0800

    closes #6

--- spec/sources.js
+++ spec/sources.js
@@ -6,25 +6,24 @@ var fs = require('fs');
 var config = require('../config/development');
 var removeFileExtension = require('../bin/string-utilities').removeFileExtension;
 
-async.each(fs.readdirSync(config.dirs.sources), function(file, callback) {
-  describe('Checking source ' + file, function() {
-    //at least 30 seconds
+function getOneTable(src) {
+  var dir = fs.readdirSync(config.dirs.input + src);
+  var tables = dir.filter(function(v) {
+    if (v.indexOf('.') == 0) return false;
+    return true;
+  });
+  return removeFileExtension(tables[0]);
+}
+
+
+//if --source passed, only test one
+if (process.argv.length == 4) {
+  var source = process.argv[3].replace(/-/g, '');
+  describe('Checking only one source: ' + source, function() {
     this.timeout(30000);
-
-    //remove file extension
-    var source = removeFileExtension(file);
-
-    //change to production
     process.env.NODE_ENV = "production";
 
-    //which table?
-    var tableDir = fs.readdirSync(config.dirs.input + source);
-    var tables = tableDir.filter(function(value) {
-      if (value.indexOf('.') == 0) return false;
-      return true;
-    });
-
-    var table = removeFileExtension(tables[0]);
+    var table = getOneTable(source);
 
     it("Should run a bridge", function(done) {
       bridge(config, {
@@ -44,8 +43,43 @@ async.each(fs.readdirSync(config.dirs.sources), function(file, callback) {
         }
       })
     })
-
   });
+  //quit
+  return;
+}
 
 
-})
+// all sources
+async.each(fs.readdirSync(config.dirs.sources), function(file, callback) {
+  describe('Checking source ' + file, function() {
+    //at least 30 seconds
+    this.timeout(30000);
+
+    //remove file extension
+    var source = removeFileExtension(file);
+
+    //change to production
+    process.env.NODE_ENV = "production";
+
+    var table = getOneTable(source);
+
+    it("Should run a bridge", function(done) {
+      bridge(config, {
+        source: source,
+        destination: 'mssql',
+        binds: true,
+        task: true,
+        update: false,
+        table: table
+      }, function(err) {
+        if (err) {
+          assert(false, err);
+          done();
+        } else {
+          assert(true);
+          done();
+        }
+      });
+    });
+  });
+});
