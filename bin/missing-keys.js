c16896292c9c99f50cc7d5ec222af3089b79d1f8
Michael Smith <psalmody@gmail.com>
2016-04-27 12:23:02 -0800

    fixed issue with passing empty array

--- bin/missing-keys.js
+++ bin/missing-keys.js
@@ -6,7 +6,8 @@ module.exports = function missingKeys(obj, arr) {
   //if not an object - return
   if (typeof(obj) !== 'object') throw new Error('missing-keys: First parameter must be an object.');
   //array required
-  if (typeof(arr) !== 'object' || !arr.length) throw new Error('missing-keys: Second parameter must be an array.');
+  if (typeof(arr) !== 'object' || !arr instanceof Array) throw new Error('missing-keys: Second parameter must be an array.');
+
   var missing = [];
   //loop through arr and check for those object properties
   for (var i = 0; i < arr.length; i++) {
