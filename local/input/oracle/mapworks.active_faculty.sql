39a2b7a5edd2eeddd461faaff23b6be360ca6019
Michael Smith <psalmody@gmail.com>
2016-05-17 10:59:01 -0800

    fix duplicate error on import

--- local/input/oracle/mapworks.active_faculty.sql
+++ local/input/oracle/mapworks.active_faculty.sql
@@ -1,14 +1,14 @@
 SELECT DISTINCT AA.EMPLOYEE_ID AS "ExternalId",
-       GOBTPAC.GOBTPAC_EXTERNAL_USER AS "AuthUsername",
-       AA.NAME_FIRST AS "Firstname",
-       AA.EMP_NAME_LAST AS "Lastname",
-       AA.JOB_TITLE AS "Title",
-       1 AS "IsActive",
-       GOBTPAC.GOBTPAC_EXTERNAL_USER || '@uaa.alaska.edu' AS "PrimaryEmail"
+                GOBTPAC.GOBTPAC_EXTERNAL_USER AS "AuthUsername",
+                AA.NAME_FIRST AS "Firstname",
+                AA.EMP_NAME_LAST AS "Lastname",
+                AA.JOB_TITLE AS "Title",
+                1 AS "IsActive",
+                GOBTPAC.GOBTPAC_EXTERNAL_USER || '@uaa.alaska.edu' AS "PrimaryEmail"
   FROM DSDMGR.DSD_ACTIVE_ASSIGNMENTS AA
        LEFT JOIN SPRIDEN
-          ON     SPRIDEN.SPRIDEN_ID = AA.EMPLOYEE_ID
-             AND SPRIDEN.SPRIDEN_CHANGE_IND IS NULL
+         ON     SPRIDEN.SPRIDEN_ID = AA.EMPLOYEE_ID
+            AND SPRIDEN.SPRIDEN_CHANGE_IND IS NULL
        LEFT JOIN GOBTPAC ON SPRIDEN.SPRIDEN_PIDM = GOBTPAC.GOBTPAC_PIDM
  WHERE     1 = 1
        AND AA.JOB_TKL BETWEEN 'T600' AND 'T799'
@@ -17,3 +17,4 @@ SELECT DISTINCT AA.EMPLOYEE_ID AS "ExternalId",
                            'F9',
                            'FT',
                            'FW')
+       AND AA.JOB_TYPE = 'P'
