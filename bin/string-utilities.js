b496bb7994a7a0c307dfe4fed0a468f49c53ffee
Michael Smith <psalmody@gmail.com>
2016-05-23 14:35:55 -0800

    fix issue with filenames that have no extension

--- bin/string-utilities.js
+++ bin/string-utilities.js
@@ -1,6 +1,7 @@
 function removeFileExtension(str) {
   if (typeof(str) !== 'string') throw new Error('removeFileExtension: Parameter must be a string.');
   var s = str.split('.');
+  if (s.length == 1) return s;
   s.pop();
   return s.join('.');
 }
