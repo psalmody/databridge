7052862b51fd44da81a176b39ba780441960e4ea
Michael Smith <psalmody@gmail.com>
2016-05-24 12:18:09 -0800

    allow for longer DEC values

--- bin/col-parser.js
+++ bin/col-parser.js
@@ -9,13 +9,13 @@ module.exports = function(opfile, callback) {
   //try and parse SQL data type from javascript data type
   function getType(c, o) {
     //if GPA is in column title - it's a decimal
-    if (c.toUpperCase().indexOf('GPA') > -1) return 'DECIMAL(4,2) NULL';
+    if (c.toUpperCase().indexOf('GPA') > -1) return 'DECIMAL(8,2) NULL';
     //if DATE in column title - assume date
     if (c.toUpperCase().indexOf('DATE') > -1) return 'DATE NULL';
     //if TIMESTAMP in column title - assume date
     if (c.toUpperCase().indexOf('TIMESTAMP') > -1) return 'DATE NULL';
     //if _DEC assume decimal
-    if (c.toUpperCase().indexOf('_DEC') > -1) return 'DECIMAL(4,2) NULL';
+    if (c.toUpperCase().indexOf('_DEC') > -1) return 'DECIMAL(8,2) NULL';
     //if number - return as INT
     if (typeof(o) == 'number') return 'INT NULL';
     //default is a 255 length VARCHAR
