6efcd349022a08adbbf8f5ebd7d7f40bda5dfbe9
Michael Smith <psalmody@gmail.com>
2016-07-26 11:48:00 -0800

    consolidating

--- bin/npm-ls.js
+++
@@ -1,7 +0,0 @@
-//outputs npm ls --json
-module.exports = function(moduleCallback) {
-  require('child_process').exec('npm ls --json', function(err, stdout, stderr) {
-    if (err) return moduleCallback(err)
-    moduleCallback(null, JSON.parse(stdout));
-  });
-}
