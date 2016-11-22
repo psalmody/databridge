f8e99d0d154b44265207efe34cd59aad4ae82f3d
Michael Smith <psalmody@gmail.com>
2016-11-22 12:02:47 -0900

    lint and spec/batches bugfix

--- bin/missing-keys.js
+++ bin/missing-keys.js
@@ -6,7 +6,7 @@ module.exports = function missingKeys(obj, arr) {
   //if not an object - return
   if (typeof(obj) !== 'object') throw new Error('missing-keys: First parameter must be an object.');
   //array required
-  if (typeof(arr) !== 'object' || !arr instanceof Array) throw new Error('missing-keys: Second parameter must be an array.');
+  if (typeof(arr) !== 'object' || !(arr instanceof Array)) throw new Error('missing-keys: Second parameter must be an array.');
 
   var missing = [];
   //loop through arr and check for those object properties
--- spec/destinations.js
+++ spec/destinations.js
@@ -9,6 +9,7 @@ var destinations = require('../bin/list-dest')(config);
 var colParser = require('../bin/col-parser');
 var columns;
 var Timer = require('../bin/timer');
+var fs = require('fs');
 opt.log = require('../bin/log-test')(Object.assign({}, config, opt));
 opt.timer = new Timer();
 opt.source = 'DB_TEST';
