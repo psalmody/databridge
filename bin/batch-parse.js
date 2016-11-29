4a3d93acca5e416cea50f0163a152a7d403e8117
Michael Smith <psalmody@gmail.com>
2016-11-29 12:24:56 -0900

    #48 for batch

--- bin/batch-parse.js
+++ bin/batch-parse.js
@@ -10,16 +10,34 @@ module.exports = function(batchName, batchFile) {
     //always use tasks unless specifically noted as not
     if (Object.keys(b).indexOf('task') === -1) b.task = true;
     b.batch = batchName;
-    var fn = (function() {
-      return function(responses, cb) {
-        bridge(config, b, function(err, response) {
-          if (err) return cb(err);
-          //push clean version (no methods) of response
-          responses.push(response.strip());
-          cb(null, responses);
-        });
-      };
-    })(b);
+    //assume it's a bridge, not a script
+    if (Object.keys(b).indexOf('type') === -1) b.type = 'bridge';
+    //handle script type
+    if (options.type == 'script') {
+      var fn = (function() {
+        var script = require(config.dirs.input + b.name);
+        return function(responses, cb) {
+          script(config, b, function(err, response) {
+            if (err) return cb(err);
+            //push clean version (no methods) of response
+            responses.push(response);
+            cb(null, responses);
+          });
+        };
+      })(b);
+    } else if (options.type == 'bridge') {
+      var fn = (function() {
+        return function(responses, cb) {
+          bridge(config, b, function(err, response) {
+            if (err) return cb(err);
+            //push clean version (no methods) of response
+            responses.push(response.strip());
+            cb(null, responses);
+          });
+        };
+      })(b);
+    }
+
     bridges.push(fn);
   });
   return bridges;
