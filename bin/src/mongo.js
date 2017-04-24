e18f41288bf319324160c7aaf98dff3ec0793c6f
Michael Tallino-Smith <psalmody@gmail.com>
2017-04-24 15:16:56 -0800

    closes #26

--- bin/src/mongo.js
+++ bin/src/mongo.js
@@ -31,13 +31,31 @@ module.exports = function(opt, moduleCallback) {
           cb(null, docs);
         });
       },
-      //TODO should append _IND or _DEC to columns with index or decimal?
+      function(dataDocs, cb) {
+        collection.indexInformation(function(err, indexes) {
+          cb(null, dataDocs, indexes);
+        })
+      },
+      //TODO should append _DEC to columns with decimal?
       // or use Mongo query files?
-      function(docs, cb) {
+      function(docs, indexes, cb) {
         //handle columns
         cols = Object.keys(docs[0]).filter(function(a) {
           return a !== '_id';
         });
+        //handle indexes
+        for (p in indexes) {
+          if (cols.indexOf(indexes[p][0][0]) > -1) cols[cols.indexOf(indexes[p][0][0])] = indexes[p][0][0] + '_IND';
+        }
+        //check for decimals and append _DEC if not _GPA or _DEC already and not a whole number
+        // obviously this is possibly a problem if not all numbers in the column are decimal
+        var row = docs[1]
+        for (var k in row) {
+          if (typeof(row[k]) === 'number' && Math.floor(row[k]) !== row[k] &&
+            cols[cols.indexOf(k)].indexOf('_DEC') === -1 && cols[cols.indexOf(k)].indexOf('_GPA') === -1) {
+            cols[cols.indexOf(k)] = cols[cols.indexOf(k)] + '_DEC';
+          }
+        }
         opfile.append(cols.join('\t') + '\n', function(err) {
           if (err) return cb(err);
           cb(null, docs);
