a4bee9ec3332b25edadbef6e0393642e351e6d0c
Michael Tallino-Smith <psalmody@gmail.com>
2017-05-04 10:09:36 -0800

    /xlsx: WIP - waiting for SheetJS/js-xlsx#647

--- bin/dest/xlsx.js
+++ bin/dest/xlsx.js
@@ -1,5 +1,6 @@
 module.exports = (opt, columns, moduleCallback) => {
 
+  //file locations
   let dt = new Date()
   let dir = dt.getFullYear() + '-' + ('0' + (Number(dt.getMonth()) + 1).toString()).slice(-2) + '-' + ('0' + dt.getDate()).slice(-2)
 
@@ -9,17 +10,42 @@ module.exports = (opt, columns, moduleCallback) => {
   const outputFile = `${opt.cfg.dirs.output}xlsx/${dir}/${table}.xlsx`
   const XLSX = require('xlsx')
 
-  let wb = XLSX.readFile(opt.opfile.filename, {FS: '\t'})
+  XLSX.cellDates = true
+
+  //read tsv file
+  let wb = XLSX.readFile(/*opt.opfile.filename*/'./local/input/tsv/SHORT_MOCK.txt', {
+    FS: '\t',
+    cellDates: true
+  })
   let worksheet = wb.Sheets['Sheet1']
 
-  console.log(wb.SheetNames)
+  //get header row
+  let headers = []
+  let range = XLSX.utils.decode_range(worksheet['!ref'])
 
+  let C
+  let R = range.s.r /* start in the first row */
 
+  //loop through first row
+  for (C = range.s.c; C <= range.e.c; ++C) {
+    let v = XLSX.utils.encode_cell({
+      c: C,
+      r: R
+    })
+    let cell = worksheet[v]
+    let hdr = "UNKNOWN " + C
+    if (cell && cell.t) {
+      worksheet[v].v = worksheet[v].v.replace(/_IND|_DEC/, '')
+      hdr = XLSX.utils.format_cell(cell)
+    }
+    headers.push(hdr)
+  }
 
+  //mkdirp and write file
   mkdirp(opt.cfg.dirs.output + 'xlsx/' + dir, (e) => {
     if (e) return moduleCallback(e)
-    XLSX.writeFile(wb, outputFile)
-    moduleCallback(null)
+    XLSX.writeFile(wb, outputFile, {cellDates: true})
+    moduleCallback(null, range.e.r, headers)
   })
 
 }
--- bin/src/xlsx.js
+++ bin/src/xlsx.js
@@ -8,22 +8,43 @@ module.exports = (opt, moduleCallback) => {
   const opfile = opt.opfile
   const async = require('async')
 
-  let workbook = XLSX.readfile(opt.table)
+  let workbook = XLSX.readFile(filename)
 
   let worksheet = workbook.Sheets[workbook.SheetNames[0]]
 
   let output = XLSX.utils.sheet_to_csv(worksheet, {
-    FS: '\t'
+    FS: '\t',
+    cellDates: true
   })
 
-  console.log(worksheet['!cols'])
+  //get header row
+  let headers = []
+  let range = XLSX.utils.decode_range(worksheet['!ref'])
+
+  let C
+  let R = range.s.r /* start in the first row */
+
+  //loop through first row
+  for (C = range.s.c; C <= range.e.c; ++C) {
+    let v = XLSX.utils.encode_cell({
+      c: C,
+      r: R
+    })
+    let cell = worksheet[v]
+    let hdr = "UNKNOWN " + C
+    if (cell && cell.t) {
+      worksheet[v].v = worksheet[v].v.replace(/_IND|_DEC/, '')
+      hdr = XLSX.utils.format_cell(cell)
+    }
+    headers.push(hdr)
+  }
 
   opfile.append(output, (e)=> {
     if (e) {
       log.error(e)
       return moduleCallback(e)
     }
-    moduleCallback(null)
+    moduleCallback(null, range.e.r, headers)
   })
 
 }
