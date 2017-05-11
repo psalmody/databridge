26629e40865c2d4346c59d5193991ab63c174f59
Michael Tallino-Smith <psalmody@gmail.com>
2017-05-11 15:24:19 -0800

    #68 for mssql

--- bin/dest/mssql.js
+++ bin/dest/mssql.js
@@ -1,161 +1,167 @@
-module.exports = function(opt, columns, moduleCallback) {
+module.exports = (opt, columns, moduleCallback) =>  {
 
-  var mssql = require('mssql'),
-    readline = require('readline'),
-    creds = require(opt.cfg.dirs.creds + 'mssql'),
-    async = require('async'),
-    log = opt.log,
-    opfile = opt.opfile;
+  const mssql = require('mssql')
+  const readline = require('readline')
+  const creds = require(opt.cfg.dirs.creds + opt.destination)
+  const async = require('async')
+  let log = opt.log
+  let opfile = opt.opfile
 
-  var db = opt.source,
+  let db = opt.source
     //use default dbo unless schema in filenamenode_modules
-    table = opt.table.indexOf('.') > -1 ? opt.table : 'dbo.' + opt.table,
-    schema = table.split('.')[0];
+  let table = opt.table.indexOf('.') > -1 ? opt.table : 'dbo.' + opt.table
+  let schema = table.split('.')[0]
 
   function sqlTable() {
-    var cols = [],
-      ndxs = [];
-    for (var i = 0; i < columns.length; i++) {
-      cols.push(' ' + columns[i].name + ' ' + columns[i].type + ' NULL');
-      if (columns[i].index) ndxs.push(' INDEX ' + columns[i].name + ' (' + columns[i].name + ') ');
-    }
-    var sql = 'CREATE TABLE ' + table + ' ( ' + cols.join(', ');
-    if (ndxs.length) sql += ', ' + ndxs.join(',');
-    sql += ' )';
-    return sql;
+    let cols = []
+    let ndxs = []
+    columns.forEach((c) => {
+      cols.push(` ${c.name} ${c.type} NULL `)
+      if (c.index) ndxs.push(` INDEX ${c.name} ( ${c.name} ) `)
+    })
+    let sql = 'CREATE TABLE ' + table + ' ( ' + cols.join(', ')
+    if (ndxs.length) sql += ', ' + ndxs.join(',')
+    sql += ' )'
+    return sql
   }
 
   async.waterfall([
     //connect
-    function(cb) {
-      mssql.connect(creds).then(function() {
-        cb(null);
-      }).catch(function(err) {
-        cb('Connect error: ' + err);
-      });
+    (cb) =>  {
+      mssql.connect(creds, (err) =>  {
+        if (err) return cb('Connect error: ' + err)
+        cb(null)
+      })
     },
     //create database if necessary
-    function(cb) {
+    (cb) =>  {
+      let s = `if not exists(select * from sys.databases where name = '${db}') create database ${db}`
       new mssql
         .Request()
-        .query('if not exists(select * from sys.databases where name = \'' + db + '\') create database ' + db)
-        .then(function() {
-          cb(null);
-        }).catch(function(err) {
-          cb('Create db error: ' + err);
-        });
+        .query(s, (err) =>  {
+          if (err) return cb(err)
+          cb(null)
+        })
     },
     //create schema if necessary
-    function(cb) {
+    (cb) =>  {
+      let s = `USE ${db} IF (SCHEMA_ID('${schema}') IS NULL ) BEGIN EXEC ('CREATE SCHEMA [${schema}] AUTHORIZATION [dbo]') END`
       new mssql
         .Request()
-        .query('USE ' + db + '; IF (SCHEMA_ID(\'' + schema + '\') IS NULL ) BEGIN EXEC (\'CREATE SCHEMA [' + schema + '] AUTHORIZATION [dbo]\') END')
-        .then(function() {
-          cb(null);
-        }).catch(function(err) {
-          cb('Create schema error: ' + err);
-        });
+        .query(s, (e) => {
+          if (e) return cb(e)
+          cb(null)
+        })
     },
     //drop table if exists
-    function(cb) {
-      if (opt.update) {
-        return cb(null); //don't drop table if update option
-      }
-      var sql = 'USE ' + db + '; IF OBJECT_ID(\'' + table + '\') IS NOT NULL DROP TABLE ' + table;
-      new mssql.Request().query(sql).then(function() {
-        cb(null);
-      }).catch(function(err) {
-        cb('Drop table error: ' + err);
-      });
+    (cb) =>  {
+      //don't drop table if update or truncate
+      if (opt.update || opt.truncate) return cb(null)
+      let sql = `USE ${db} IF OBJECT_ID('${table}') IS NOT NULL DROP TABLE ${table}`
+      new mssql.Request().query(sql, (e) =>  {
+        if (e) return cb('Drop table error: ' + e)
+        cb(null)
+      })
     },
     //create table
-    function(cb) {
-      if (opt.update) return cb(null); //don't drop table if update option
-      var sql = sqlTable();
-      new mssql.Request().query('USE ' + db + ';' + sql).then(function() {
-        cb(null);
-      }).catch(function(err) {
-        cb('Create table error: ' + err);
-      });
+    (cb) =>  {
+      //don't drop table if update or truncate
+      if (opt.update || opt.truncate) return cb(null)
+      new mssql.Request().query(`USE ${db} ` + sqlTable(), (e) => {
+        if (e) return cb('Create table error: ' + e)
+        cb(null)
+      })
+    },
+    //truncate table if specified
+    (cb) => {
+      if (!opt.truncate) return cb(null)
+      new mssql.Request().query('TRUNCATE TABLE ' + table, (e) => {
+        if(e instanceof Error && e.toString().indexOf('because it does not exist') !== -1) {
+          //if table doesn't exist, make it
+          console.log('creating missing table')
+          new mssql.Request().query(`USE ${db} ` + sqlTable(), (e) => {
+            if (e) return cb('Create table that didnt exist to truncate error: ' + e)
+            cb(null)
+          })
+        } else {
+          if (e) return cb(e)
+          cb(null)
+        }
+      })
     },
     //create insert statement
-    function(cb) {
-      var sql = 'USE ' + db + '; INSERT INTO ' + table + ' ';
-      var cs = [];
-      var first = true;
-      columns.forEach(function(c) {
-        cs.push(c.name);
-      });
-      sql += ' ( ' + cs.join(', ') + ' ) VALUES ';
-      var lineReader = readline.createInterface({
+    (cb) =>  {
+      let sql = `USE ${db} INSERT INTO ${table} `
+      let cs = []
+      let first = true
+      columns.forEach((c) =>  {
+        cs.push(c.name)
+      })
+      sql += ` ( ${cs.join(', ')} ) VALUES `
+      let lineReader = readline.createInterface({
         input: opfile.createReadStream()
-      });
-      lineReader.on('error', function(err) {
-        return cb('Readline error: ' + err);
-      });
-      var insertLines = [];
-      lineReader.on('line', function(line) {
+      })
+      lineReader.on('error', (err) =>  {
+        return cb('Readline error: ' + err)
+      })
+      let insertLines = []
+      lineReader.on('line', (line) =>  {
         if (first) {
-          first = false;
+          first = false
         } else {
-          var l = ' (\'' + line.replace(/\'/g, '').split('\t').join('\', \'') + '\') ';
-          l = l.replace(/\'\'/g, 'NULL').replace(/(\'[0-9]+\.[0-9]+\'|\'[0-9]\')/g, '$1');
-          insertLines.push(l);
+          let l = ' (\'' + line.replace(/\'/g, '').split('\t').join('\', \'') + '\') '
+          l = l.replace(/\'\'/g, 'NULL').replace(/(\'[0-9]+\.[0-9]+\'|\'[0-9]\')/g, '$1')
+          insertLines.push(l)
         }
-      });
-      lineReader.on('close', function() {
-        cb(null, sql, insertLines);
-      });
+      })
+      lineReader.on('close', () =>  {
+        cb(null, sql, insertLines)
+      })
     },
     //insert lines
-    function(sql, lines, cb) {
-      var batchCount = Math.ceil(lines.length / 500);
-      var arr = [];
-      for (var i = 0; i < batchCount; i++) {
-        arr.push(i + 1);
+    (sql, lines, cb) =>  {
+      let batchCount = Math.ceil(lines.length / 500)
+      let arr = []
+      for (let i = 0; i < batchCount; i++) {
+        arr.push(i + 1)
       }
-      async.map(arr, function(i, callback) {
-        var stmt = sql + lines.slice(i * 500 - 500, i * 500).join(', ');
-        new mssql.Request().query(stmt).then(function() {
-          callback(null);
-        }).catch(function(err) {
-          callback('Insert values error: ' + err);
-        });
-      }, function(err) {
-        if (err) return cb(err);
-        cb(null);
-      });
+      async.map(arr, (i, callback) =>  {
+        let stmt = sql + lines.slice(i * 500 - 500, i * 500).join(', ')
+        new mssql.Request().query(stmt, (err) => {
+          if (err) return callback('Insert values error: ' + err)
+          callback(null)
+        })
+      }, (err) =>  {
+        if (err) return cb(err)
+        cb(null)
+      })
     },
     //check number of inserted rows
-    function(cb) {
-      new mssql.Request().query('USE ' + db + '; SELECT count(*) as rows FROM ' + table).then(function(recordset) {
-        cb(null, recordset[0].rows);
-      }).catch(function(err) {
-        cb('Check row number error: ' + err);
-      });
+    (cb) =>  {
+      new mssql.Request().query(`USE ${db} SELECT count(*) as rows FROM ${table}`, (err, recordset) =>  {
+        if (err) return cb('Check row number error: ' + err)
+        cb(null, recordset[0].rows)
+      })
     },
-    function(rows, cb) {
-      var sql = 'USE ' + db + '; SELECT COLUMN_NAME col FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = \'' + table.split('.')[1] + '\' AND TABLE_SCHEMA = \'' + schema + '\'';
+    (rows, cb) =>  {
+      let sql = `USE ${db} SELECT COLUMN_NAME col FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = '${table.split('.')[1]}' AND TABLE_SCHEMA = '${schema}'`
       new mssql.Request()
-        .query(sql)
-        .then(function(recordset) {
-          var columns = [];
-          recordset.forEach(function(row) {
-            columns.push(row.col);
-          });
-          cb(null, rows, columns);
-        }).catch(function(err) {
-          cb(err);
-        });
-
+        .query(sql, (err, recordset) =>  {
+          if (err) return cb(err)
+          let columns = []
+          recordset.forEach((row) =>  {
+            columns.push(row.col)
+          })
+          cb(null, rows, columns)
+        })
     }
-  ], function(err, rows, columns) {
+  ], (err, rows, columns) =>  {
     try {
-      mssql.close();
+      mssql.close()
     } catch (e) {
-      log.error(e);
+      log.error(e)
     }
-    if (err) return moduleCallback(err);
-    moduleCallback(null, rows, columns);
-  });
-};
+    if (err) return moduleCallback(err)
+    moduleCallback(null, rows, columns)
+  })
+}
