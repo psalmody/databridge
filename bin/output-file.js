5480b34e2fe2c6b0b546289145207355d0827a88
Michael Tallino-Smith <psalmody@gmail.com>
2017-09-17 12:11:06 -0700

    outputfile class now

--- bin/output-file.js
+++ bin/output-file.js
@@ -2,100 +2,54 @@
  * handles the output file created for data between
  * source and destination
  */
-module.exports = function(opt, moduleCallback) {
-  //options are required
-  if (typeof(opt) == 'undefined') return moduleCallback('No options specified.');
-  //setup and require
-  var FILE = new Object(),
-    fs = require('graceful-fs'),
-    dir = opt.cfg.dirs.output;
 
-  //filename stores the complete filename for writing to later
-  FILE.filename = dir + opt.table + '.' + Math.round(Date.now() / 1000) + '.dat';
+const fs = require('fs')
 
-  //cleanup (remove) output file
-  FILE.clean = function(callback) {
-    //define callback
-    callback = typeof(callback) == 'undefined' ? function() {
-      return;
-    } : callback;
-    //delete file
-    fs.unlink(FILE.filename, function(err) {
-      if (err) return callback(err);
-      callback(null, true);
-    });
-  };
-
-  //append function - append data to file
-  FILE.append = function(data, callback) {
-    callback = typeof(callback) == 'undefined' ? function() {
-      return;
-    } : callback;
-    //append data and return callback
-    fs.appendFile(FILE.filename, data, function(err) {
-      if (err) return callback(err);
-      callback(null);
-    });
-  };
-
-  //get first line for column titles
-
-
-  //get first two lines of data for column definitions
-  FILE.twoLines = function(callback) {
-    callback = typeof(callback) == 'undefined' ? function() {
-      return;
-    } : callback;
-    fs.readFile(FILE.filename, 'utf-8', function(err, data) {
-      if (err) return callback(err);
-      var two = data.replace(/\r/g, '').split('\n').slice(0, 2);
-      callback(null, two);
-    });
-  };
-
-  //sample up to ten lines from the file for column parser
-  FILE.sampleLines = function(callback) {
-    callback = typeof(callback) == 'undefined' ? function() {
-      return;
-    } : callback;
-    fs.readFile(FILE.filename, 'utf-8', function(err, data) {
-      if (err) return callback(err);
-      var returnArray = []
-      var lines = data.replace(/\r/g, '').split('\n')
-
-      var times = Math.min(lines.length - 1, 100)
-
-      for (var i = 0; i < times; i++) {
+class OutputFile {
+  //set filename and create file
+  constructor(opt) {
+    const dir = opt.cfg.dirs.output
+    this.filename = `${dir}${opt.table}.${Date.now()}.dat`
+    fs.openSync(this.filename, 'w')
+    return;
+  }
+  //remove file - cleanup
+  clean(callback = () => {return}) {
+    fs.unlink(this.filename, (err) => {
+      return (err) ? callback(err) : callback(null, true)
+    })
+  }
+  //append stuff to file
+  append(data, callback = () => {return}) {
+    fs.appendFile(this.filename, data, (err) => {
+      return (err) ? callback(err) : callback(null)
+    })
+  }
+  //get first two lines
+  twoLines(callback) {
+    fs.readFile(this.filename, 'utf-8', (err, data) => {
+      return (err) ? callback(err) : callback(null, data.replace(/\r/g, '').split('\n').slice(0,2))
+    })
+  }
+  //sample 100 lines
+  sampleLines(callback) {
+    fs.readFile(this.filename, 'utf-8', (err, data) => {
+      if (err) return callback(err)
+      let lines = data.replace(/\r/g, '').split('\n')
+      const times = Math.min(lines.length - 1, 100)
+      let returnArray = []
+      for(let i = 0; i < times; i++) {
         returnArray.push(lines[Math.floor(Math.random() * (lines.length - 1)) + 2])
       }
-      //returns no error, random sampling of up to 100 lines (or all the lines if less than 100) and column names in array
       callback(null, returnArray, lines[0].split('\t'))
-    });
-  };
+    })
+  }
+  get writeStream() {
+    return fs.createWriteStream(this.filename)
+  }
+  get readStream() {
+    return fs.createReadStream(this.filename)
+  }
+}
 
-  //stream options
-  FILE.createWriteStream = function() {
-    return fs.createWriteStream(FILE.filename);
-  };
-  FILE.createReadStream = function() {
-    return fs.createReadStream(FILE.filename);
-  };
-
-
-  //create file
-  fs.open(FILE.filename, 'w', function(err, fd) {
-    if (err) {
-      console.log(err);
-      return moduleCallback(err);
-    }
-    //close it now that it's created
-    fs.close(fd, function(err) {
-      if (err) {
-        console.log(err);
-        return moduleCallback(err);
-      }
-      moduleCallback(null, FILE);
-    });
-  });
-  return FILE;
-};
+module.exports = OutputFile
