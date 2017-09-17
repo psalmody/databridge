762b49b2943f1fe3dea7eb58052a4b66b0e00f16
Michael Tallino-Smith <psalmody@gmail.com>
2017-09-17 12:10:45 -0700

    es6 class

--- bin/timer.js
+++ bin/timer.js
@@ -2,32 +2,23 @@
  * Timer module to be passed between source and destination
  * in order to time entire process.
  */
-var Timer = function() {
-  this.s = Date.now();
-};
 
-function seconds(t) {
-  return (t / 1000).toFixed(2);
+class Timer {
+  constructor() {
+    this.s = Date.now()
+  }
+  get seconds() {
+    return ((Date.now() - this.s) / 1000).toFixed(2)
+  }
+  get minutes() {
+    return ((Date.now() - this.s) / 1000 /60).toFixed(2)
+  }
+  get str() {
+    return `Since timer start: ${this.seconds} seconds / ${this.minutes} minutes`
+  }
+  get start() {
+    return this.s.toString()
+  }
 }
 
-function minutes(t) {
-  return (t / 1000 / 60).toFixed(2);
-}
-
-Timer.prototype.sec = function() {
-  return seconds(Date.now() - this.s);
-};
-
-Timer.prototype.min = function() {
-  return minutes(Date.now() - this.s);
-};
-
-Timer.prototype.str = function() {
-  return 'Since timer start: ' + this.sec() + ' seconds / ' + this.min() + ' minutes.';
-};
-
-Timer.prototype.start = function() {
-  return this.s.toString();
-};
-
-module.exports = Timer;
+module.exports = Timer
