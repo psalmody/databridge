385cd618b353117f08879f76f859eb7835635e5d
Michael Tallino-Smith <psalmody@gmail.com>
2017-09-17 10:32:44 -0700

    es6

--- bin/bind-query.js
+++ bin/bind-query.js
@@ -1,20 +1,20 @@
 module.exports = function(query, opt, moduleCallback) {
 
-  var missingKeys = require('./missing-keys');
 
   //read query, check binds, prompt or use defBinds
-  var qBinds = [],
+  let qBinds = []
     //remove comments
-    sql = query.replace(/\/\*[.\s\S]*?\*\/|--.*?[\n\r]/g, ''),
+  let sql = query.replace(/\/\*[.\s\S]*?\*\/|--.*?[\n\r]/g, '')
     //find binds
     //bindPattern = /[:].[a-z_0-9]+/g;
     //now bind pattern ignores : inside quotes (date formats)
-    bindPattern = /[:].[A-Za-z_0-9]+(?=([^']*'[^']*')*[^']*$)/g,
+  const bindPattern = /[:].[A-Za-z_0-9]+(?=([^']*'[^']*')*[^']*$)/g
     //extend config binds with any attached to opt
-    defBinds = Object.assign({}, opt.cfg.defaultBindVars, opt.binds),
-    spinner = opt.spinner,
-    result;
+  const defBinds = Object.assign({}, opt.cfg.defaultBindVars, opt.binds)
+  const spinner = opt.spinner
 
+  //loop through query looking for binds
+  let result
   while ((result = bindPattern.exec(sql))) {
     var r = result[0].replace(':', '');
     if (qBinds.indexOf(r) == -1) qBinds.push(r);
@@ -23,48 +23,47 @@ module.exports = function(query, opt, moduleCallback) {
   //if no binds in query, return the query file
   if (!qBinds.length) return moduleCallback(null, sql, defBinds);
 
-  function replaceBinds(query, binds) {
-    for (var i = 0; i < qBinds.length; i++) {
+  const replaceBinds = (query, binds) => {
+    qBinds.forEach(v => {
       try {
         //try replacing all binds with default
-        query = query.replace(new RegExp(':' + qBinds[i], 'g'), binds[qBinds[i]]);
+        query = query.replace(new RegExp(':' + v, 'g'), binds[v]);
       } catch (e) {
         return moduleCallback('Problem finding bind for "' + r + '"' + e);
       }
-    }
+    })
     return query;
   }
 
   //check to see if ALL binds are present in defBinds AND opt.binds is present
-  if (opt.binds && missingKeys(defBinds, qBinds) == false) {
+  if (opt.binds && qBinds.every(v => Object.keys(defBinds).indexOf(v) >= 0)) {
     //return formatted query
     var q = replaceBinds(sql, defBinds);
     return moduleCallback(null, q, defBinds);
   }
 
   //if not using defBinds, prompt
-  var prompt = require('prompt'),
-    colors = require('colors'),
-    prompts = {
+  const prompt = require('prompt')
+  const colors = require('colors')
+  let prompts = {
       properties: {
 
       }
-    };
+    }
 
-  prompt.message = colors.green('Enter bind variable');
+  prompt.message = colors.green('Enter bind variable')
+  qBinds.forEach(b => {
+    prompts.properties[b] = {
+      default: defBinds[b],
+      description: colors.green(b)
+    }
+  })
 
-  for (var i = 0; i < qBinds.length; i++) {
-    prompts.properties[qBinds[i]] = {
-      default: defBinds[qBinds[i]],
-      description: colors.green(qBinds[i])
-    };
-  }
-
-  if (spinner) spinner.stop(true);
-  prompt.start();
-  prompt.get(prompts, function(err, res) {
+  if (spinner) spinner.stop(true)
+  prompt.start()
+  prompt.get(prompts, (err, res) => {
     if (err) return moduleCallback(err);
-    if (spinner) spinner.start();
+    if (spinner) spinner.start()
     moduleCallback(null, replaceBinds(sql, res), res);
   });
 };
