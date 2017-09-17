f58c43279701ad16119c4d337249ec8242408fbd
Michael Tallino-Smith <psalmody@gmail.com>
2017-09-17 12:56:08 -0700

    fix script runs

--- bin/batch-parse.js
+++ bin/batch-parse.js
@@ -10,12 +10,25 @@ module.exports = (batchName, batchFile) => {
   const Bridge = require('./bridge')
   const config = require('../config.json')
 
+  class Script {
+    constructor({config, opt}) {
+      this.config = config
+      this.opt = opt
+      this.opt.cfg = config
+      this.script = require(config.dirs.input + opt.name)
+    }
+    run(callback) {
+      this.script(this.config, this.opt, callback)
+    }
+  }
+
   return batch.map((b) => {
     let o = Object.assign({}, {
       type: 'bridge',
       task: true,
       batch: batchName
     }, b)
+    if (o.type === 'script') return new Script({config: config, opt: o})
     return new Bridge({config: config, opt: o})
   })
 }
